d46b0be24a43f87ce8c8ca971a7009a2569baf43
sheapa <55968932+sheapa@users.noreply.github.com>
2020-03-06 13:26:33 -0800

    Can instatniate employee instance Pass.

--- app.js
+++ app.js
@@ -13,7 +13,66 @@ const render = require("./lib/htmlRenderer");
 ​
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: "What the employee's name?"
+        }
+        // {
+        //   type: 'input',
+        //   name: 'title',
+        //   message: 'What is the title of your project?'
+        // },
+        // {
+        //   type: 'input',
+        //   name: 'description',
+        //   message: 'Please breifly describe your project.'
+        // },
+        // {
+        //   type: 'input',
+        //   name: 'instructions',
+        //   message: 'What are you program instructions?'
+        // },
+        // {
+        //   type: 'input',
+        //   name: 'liscenses',
+        //   message: 'Please provide liscense information.'
+        // },
+        // {
+        //   type: 'input',
+        //   name: 'tests',
+        //   message: 'Provide details on your tests and how to run them.'
+        // },
+        // {
+        //   type: 'input',
+        //   name: 'acknowledgments',
+        //   message: 'This is the place to list any special shout outs to others that made this project possible.'
+        // }
+
+    ]);
+}
+
+async function init() {
+    try {
+        // Answers not avialable until after inquierer prompt.
+        const answers = await promptUser();
+        // Cannot create text for readme, unitl after answers and api data are made available and fead into readme template. 
+        // const text = await generateREADME(answers);
+        // // Readme file will be written to file system using data from text variable and the async promis wll be fullfilled.
+        // await writeFileAsync("README.md", text);
+        // // Console log success when readme is written to filesystem. Othewise error.
+        // console.log("Successfully wrote to README.md");
+      } catch(err) {
+        console.log(err);
+      }
+  }
+  // Initiates process.
+  init();
+  
+// ​
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
