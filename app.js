87bf745306b924177ee0feae255600586023c499
sheapa <55968932+sheapa@users.noreply.github.com>
2020-03-11 23:34:52 -0700

    inquier prompts added

--- app.js
+++ app.js
@@ -4,72 +4,90 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-// function getEmployee() {
-//     inquirer
-//     .prompt(menuQuestions)
-//     .then(({userChoice}) => {
-//         switch (userChoice) {
-//             case 'Engineer':
-//                 getEngineer();
-//                 break;
-//             case 'Intern':
-//                 getIntern();
-//                 break;
-//             case 'manager':
-//                 displayHtml(employeeList);
-//                 break;
-//         }
-//     })
-// }
-// ​
-// function getEngineer() {
-//     inquirer
-//     .prompt(employeeQuestions.concat(engineerQuestions))
-//     .then(({name, id, email, githubUsername}) => {
-//         let engineer = new Engineer(name, id, email, githubUsername);
-//         employeeList.push(engineer);
-//         getEmployee();
-//     })
-// }
-// ​
-// function getIntern() {
-//     inquirer
-//     .prompt(employeeQuestions.concat(internQuestions))
-//     .then(({name, id, email, school}) => {
-//         let intern = new Intern(name, id, email, school);
-//         employeeList.push(intern)
-//         getEmployee();
-//     })
-// }
+// User prompt for Git username and other . Via Inquierer
+function promptUser() {
+    return inquirer.prompt([
+        {
+          type: "list",
+          name: "employeeType",
+          message: "Employee Type: ",
+          choices: ["Manager", "Engineer", "Intern"]
+        },
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is your name?'
+        },
+        {
+          type: 'input',
+          name: 'id',
+          message: 'What is your id number?'
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: 'What is your email address?'
+        },
+        {
+          type: "input",
+          name: "github",
+          message: "What is your Github username? ",
+          when: function(answers) {
+            const value = answers.employeeType == "Engineer" ? true : false;
+            return value;
+            }
+        },
+        {
+          type: "input",
+          name: "officeNumber",
+          message: "What is your offic number? ",
+          when: function(answers) {
+            const value = answers.employeeType == "Manager" ? true : false;
+            return value;
+            }
+        },
+        {
+          type: "input",
+          name: "school",
+          message: "Which school do you attend? ",
+          when: function(answers) {
+            const value = answers.employeeType == "Intern" ? true : false;
+            return value;
+            }
+        }
+    ]);
+}
+
+// Feeds data into README.MD text format using tempelate literal values from answers & data pulled from GitHub.
+// async function generateREADME(answers) {
+//   // Cannot acces answers until inquirer prompt and api callback are completed
+//   const fullData = await api.getUser(answers);
 
   
-// ​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+//     return ` `
+    
+
+// }
+
+// Async init function for instructing stack on how to fulfill the writeFile promise.
+async function init() {
+    try {
+        // Answers not avialable until after inquierer prompt.
+        const answers = await promptUser();
+        // Cannot create text for readme, unitl after answers and api data are made available and fead into readme template. 
+        const text = await generateREADME(answers);
+        // Readme file will be written to file system using data from text variable and the async promis wll be fullfilled.
+        await writeFileAsync("README.md", text);
+        // Console log success when readme is written to filesystem. Othewise error.
+        console.log("Successfully wrote to README.md");
+      } catch(err) {
+        console.log(err);
+      }
+  }
+  // Initiates process.
+  init();
+  
